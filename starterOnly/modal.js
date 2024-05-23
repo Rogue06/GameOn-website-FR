0517c1a8e1a0394408836e29d2ad384a67cc6a62
Rogue06 <111644419+Rogue06@users.noreply.github.com>
2024-05-23 18:31:28 +0200

    style: improve responsive CSS, cleanup JS code

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -40,10 +40,8 @@ function closeModal() {
 // Ajouter un écouteur de clic à l'élément ".close" Si click alors on execute la fonction !
 closeModalCross.addEventListener("click", closeModal);
 
-// Fonction pour valider le formulaire quand l'utilisateur clique sur  "Submit"
 function validateForm(event) {
   event.preventDefault(); // Empêcher la soumission par defaut du modal
-  const form = event.target.closest("form"); // Récupérer l'élément du formulaire le plus proche
   // Soumettre le formulaire si toutes les validations sont réussies
   const isFormValid =
     validateName() &&
@@ -53,18 +51,10 @@ function validateForm(event) {
     validateRadio() &&
     validateCheckbox();
 
-  console.log("name", validateName());
-  console.log("email", validateEmail());
-  console.log("date", validateDate());
-  console.log("numbers", validateNumbers());
-  console.log("radio", validateRadio());
-  console.log("checkbox", validateCheckbox());
-  console.log("isFormValid", isFormValid);
-
   if (isFormValid === true) {
     const fieldContainer = document.querySelector(".field__container");
     const fieldContainerHeight = fieldContainer.offsetHeight;
-    fieldContainer.setAttribute("hide-modal", "true");
+    fieldContainer.setAttribute("data-hide-modal", "true");
     submitModal.style.display = "none";
     closeModalBtn.style.display = "block";
     // Récupération de la hauteur occupée par le formulaire avant de le cacher.
@@ -84,10 +74,7 @@ function validateName() {
     let inputTextValue = textInput[i].value.trim(); // Récupère la valeur du champ de texte
     let inputID = textInput[i].id; // Récupère l'ID du champ de texte
 
-    // Vérifie si l'ID correspond aux champs Prénom et Nom
     if (inputID === "first" || inputID === "last") {
-      //console.log("validation du champ " + inputID);
-
       if (
         inputTextValue === "" ||
         inputTextValue.length < 2 ||
@@ -104,29 +91,22 @@ function validateName() {
       }
     }
   }
-  //console.log("La validation du nom retourne :", isNameValid);
   return isNameValid; // Retourne l'état de la validation du nom
 }
 
-// Fonction pour tester l'email
 function validateEmail() {
   let = isEmailValid = true;
-  let inputEmailValue = emailInput.value.trim(); // Permet de voir si le champs est vide
+  let inputEmailValue = emailInput.value.trim();
   if (inputEmailValue === "" || !emailRegex.test(inputEmailValue)) {
-    // Permet de voir si l'email est valide (champ pas vide et email valide via regex)
     isEmailValid = false; // initialisation de la variable
     const formDataElement = emailInput.closest(".formData");
     formDataElement.setAttribute("data-error-visible", "true"); // Met à jour l'attribut data-error-visible à true si retour false.
-    //console.log("le champs email est vide retourne :", false);
     return false;
   } else {
-    //console.log("la validation de l'email retourne :", true);
     const formDataElement = emailInput.closest(".formData"); // Permet de trouver le formulaire
     formDataElement.setAttribute("data-error-visible", "false"); // Met à jour l'attribut data-error-visible à false si retour true.
     return true;
   }
-
-  // console.log("la validation de l'email retourne :", true);
 }
 
 // Fonction vérifier si l'utilisateur entre une date et a 18 ans ou plus
@@ -145,13 +125,11 @@ function validateDate() {
 
   // Comparer la date de naissance de l'utilisateur avec la date actuelle
   if (userBirthDate <= currentDate) {
-    //console.log("la validation de la date de naissance retourne :", true);
     // L'utilisateur a 18 ans ou plus
     const formDataElement = dateInput.closest(".formData");
     formDataElement.setAttribute("data-error-visible", "false");
     return true;
   } else {
-    //console.log("la validation de la date de naissance retourne :", false);
     // L'utilisateur a moins de 18 ans
     const formDataElement = dateInput.closest(".formData");
     formDataElement.setAttribute("data-error-visible", "true");
@@ -159,26 +137,22 @@ function validateDate() {
   }
 }
 
-// Fonction qui vérifie si le nombre de tournois est > 0
 function validateNumbers() {
   const inputNumberValue = document.querySelector(
     ".formData input[type='number']"
   );
 
   if (inputNumberValue.value <= 0) {
-    //console.log("la validation du nombre de tournois retourne :", false);
     const formDataElement = inputNumberValue.closest(".formData");
     formDataElement.setAttribute("data-error-visible", "true");
     return false;
   } else {
-    //console.log("la validation du nombre de tournois retourne :", true);
     const formDataElement = inputNumberValue.closest(".formData");
     formDataElement.setAttribute("data-error-visible", "false");
     return true;
   }
 }
 
-// Vérifier si le input de type "radio" n'est pas vide
 function validateRadio() {
   const inputRadioValue = document.querySelector(
     ".formData input[type='radio']:checked"
@@ -188,12 +162,10 @@ function validateRadio() {
     .closest(".formData");
 
   if (inputRadioValue === null) {
-    //console.log("la validation du lieu de participation retourne :", false);
     radioFormDataElement.setAttribute("data-error-visible", "true");
     return false;
   } else {
     radioFormDataElement.setAttribute("data-error-visible", "false");
-    //console.log("la validation du lieu de participation retourne :", true);
     return true;
   }
 }
@@ -203,13 +175,11 @@ function validateCheckbox() {
   if (!checkbox1Input.checked) {
     const checkbox1FormData = checkbox1Input.closest(".formData");
     checkbox1FormData.setAttribute("data-error-visible", "true");
-    //console.log("la checkbox lu et approuvé retourne :", false);
     return false;
   } else {
     checkbox1Input
       .closest(".formData")
       .setAttribute("data-error-visible", "false");
-    //console.log("la checkbox lu et approuvé retourne :", true);
     return true;
   }
 }
@@ -239,7 +209,6 @@ function displayErrorIfNeeded() {
     }
   });
 }
-
 // Récupérez tous les champs de saisie
 let textInputFields = document.querySelectorAll(".text-control");
 
